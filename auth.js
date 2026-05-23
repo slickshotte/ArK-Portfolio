a69de54d11640f28095bb5935a455bafcfbbf206
slickshotte <lozumak@gmail.com>
2026-05-23 16:56:09 +0800

    auth.js

--- auth.js
+++ auth.js
@@ -1,7 +1,9 @@
-document.addEventListener("DOMContentLoaded", () => {
+(function() {
     const hasBooted = sessionStorage.getItem('bootSequenceCompleted');
+    const isIndex = window.location.pathname.endsWith('index.html') || window.location.pathname === '/';
 
-    if (!hasBooted && window.location.pathname !== '/index.html') {
-        window.location.href = 'index.html'; 
+    // Only redirect if they haven't booted AND they are NOT already on the index page
+    if (hasBooted !== 'true' && !isIndex) {
+        window.location.href = 'index.html';
     }
-});
+})();
